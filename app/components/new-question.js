6f30617e1af5ed8622e01e10d39f92bebd791f58
Max Scher <maxobaxo@gmail.com>
2017-08-11 09:56:45 -0700

    display new-question component template

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  askNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('askNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        content: this.get('content') ? this.get('content') : ""
+      };
+      this.set('askNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
