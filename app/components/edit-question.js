272cd4976730f05e181cd7ebe340cdd28a489d86
Max Scher <maxobaxo@gmail.com>
2017-08-11 12:24:04 -0700

    add update question functionality, but encountered bug for empty updateForm field entries

--- app/components/edit-question.js
+++ app/components/edit-question.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  editQuestion: false,
+  updateQuestionForm: false,
   actions: {
-    delete(question) {
-      if(confirm('Are you sure you want to delete this question?')) {
-        this.sendAction('destroyQuestion', question);
-      }
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
     }
   }
 });
